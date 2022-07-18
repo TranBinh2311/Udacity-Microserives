5af4674d2c6716201a5d343d909c01d2a7cdac0c
TranBinh2311 <binhlinh473@gmail.com>
2022-07-18 11:17:50 -0400

    first commit

---
+++ udagram-api-feed/src/server.ts
@@ -0,0 +1,54 @@
+import cors from "cors";
+import express, { Router } from "express";
+import { sequelize } from "./sequelize";
+
+import { FeedRouter } from "./controllers/v0/index.router";
+
+import bodyParser from "body-parser";
+import { config } from "./config/config";
+import { FeedItem } from "./controllers/v0/model.index";
+
+(async () => {
+  await sequelize.addModels([FeedItem]);
+
+  console.debug("Initialize database connection...");
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8080;
+  const router: Router = Router();
+
+  app.use(bodyParser.json());
+  router.use("/feed", FeedRouter);
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS this lesson. It's
+  // something that will be covered in the next course.
+  app.use(
+    cors({
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+        "Authorization",
+      ],
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      preflightContinue: true,
+      origin: "*",
+    })
+  );
+
+  app.use("/api/v0", router);
+
+  // Root URI call
+  app.get("/", async (req, res) => {
+    res.send("/api/v0/");
+  });
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running ${config.url}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
